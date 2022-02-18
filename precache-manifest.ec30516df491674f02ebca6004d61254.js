475ac67cbd67d99e28a288d2e03ff4d0cd2d0daa
italo041 <italo041@users.noreply.github.com>
2022-02-18 04:11:12 +0000

    deploy: 82c45d07722f7f8ca0627d673845855bc3a3d359

---
+++ precache-manifest.ec30516df491674f02ebca6004d61254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645e21abc2e061d8cf4fc102aa66d510",
+    "url": "https://italo041.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8a58bcec5899ce48bd48",
+    "url": "https://italo041.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "70e8944cfa1fca5a79c3",
+    "url": "https://italo041.github.io/task-management-frontend/static/js/2.a92bb181.chunk.js"
+  },
+  {
+    "revision": "8a58bcec5899ce48bd48",
+    "url": "https://italo041.github.io/task-management-frontend/static/js/main.d14e9b51.chunk.js"
+  },
+  {
+    "revision": "610e7c2a3393db1237d0",
+    "url": "https://italo041.github.io/task-management-frontend/static/js/runtime~main.1ba931da.js"
+  }
+]);
